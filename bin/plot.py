d94d37b990338b2b6d135d959e4e4a1e697df7df
Eric Betts <bettse@gmail.com>
2011-03-15 08:44:13 -0700

    Adding a matplotlib based python script to plot ledger data.  Based on data_demo.py from the matplotlib demos

---
+++ bin/plot.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+"""
+Show how to make date plots in matplotlib using date tick locators and
+formatters.  See major_minor_demo1.py for more information on
+controlling major and minor ticks
+
+All matplotlib date plotting is done by converting date instances into
+days since the 0001-01-01 UTC.  The conversion, tick locating and
+formatting is done behind the scenes so this is most transparent to
+you.  The dates module provides several converter functions date2num
+and num2date
+
+"""
+import datetime
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import matplotlib.mlab as mlab
+import matplotlib.cbook as cbook
+from subprocess import Popen,PIPE
+import sys
+
+def usage():
+    print "Usage: " + sys.argv[0] + " <imagename> <paramters1> <parameter2> ..."
+
+years    = mdates.YearLocator()   # every year
+months   = mdates.MonthLocator()  # every month
+yearsFmt = mdates.DateFormatter('%Y')
+
+if len(sys.argv) > 2:
+    output_file = sys.argv[1]
+    parameters = sys.argv[2:]
+else:
+    usage()
+    exit()
+
+output = Popen(["ledger"] + parameters, stdout=PIPE).communicate()[0]
+times = []
+values = []
+for line in output.split('\n'):
+    if(line == ""):
+        continue
+    times.append(datetime.datetime.strptime(line.split()[0], "%Y-%m-%d"))
+    values.append(float(line.split()[1]))
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(times, values)
+
+# format the ticks
+ax.xaxis.set_major_locator(years)
+ax.xaxis.set_major_formatter(yearsFmt)
+ax.xaxis.set_minor_locator(months)
+
+datemin = datetime.date(min(times).year, 1, 1)
+datemax = datetime.date(max(times).year+1, 1, 1)
+ax.set_xlim(datemin, datemax)
+
+# format the coords message box
+def price(x): return '$%1.2f'%x
+ax.format_xdata = mdates.DateFormatter('%Y-%m-%d')
+ax.format_ydata = price
+ax.grid(True)
+
+# rotates and right aligns the x labels, and moves the bottom of the
+# axes up to make room for them
+fig.autofmt_xdate()
+
+plt.savefig(output_file+".png")
