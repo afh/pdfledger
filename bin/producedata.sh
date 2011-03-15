ca6a4520949a548ab04e812d525a22a12530d61c
Eric Betts <bettse@gmail.com>
2011-03-15 13:50:32 -0700

    Remove the production of month_breakdown.txt since pie.py calls ledger directly

--- bin/producedata.sh
+++ bin/producedata.sh
@@ -10,7 +10,7 @@ test -e $build || mkdir -p $build
 . $config
 
 #These are used to produce text output that will be verbatim included in the report
-ledger -f "${ledgerfile}" -c -E -p "this month" budget $budget_accounts > $build/budget.txt
+ledger -f "${ledgerfile}" -c -p "this month" budget $budget_accounts > $build/budget.txt
 ledger -f "${ledgerfile}" --sort d -d "d>[today]-7" -c reg $checking_1 > $build/checking-1trans.txt
 ledger -f "${ledgerfile}" --forecast "d<=[today]+365" -d "d>[today] & d<[today]+365" --sort d reg $checking_1_forecast | sed -e 's/Forecast transaction/Prognose/' > $build/checking-1forecast.txt
 ledger -f "${ledgerfile}" -w -c bal $assets_account > $build/assets.txt
@@ -24,10 +24,5 @@ python $bin/plot.py "${build}/${bank}checking-1monthbalance" -f "${ledgerfile}" 
 python $bin/plot.py "${build}/${bank}checking-1yearbalance" -f "${ledgerfile}" -J -c --weekly -d "d<=[today] & d > [today]-365" --sort d reg $checking_1_forecast
 
 python $bin/plot.py "${build}/${bank}checking-1forecast" -f "${ledgerfile}" -J -w --forecast "d<=[today]+365" -d "d>=[next month] & d<=[today]+365" --sort d reg $checking_1_forecast
-
-ledger -f "${ledgerfile}" -w -c -F "%(account)\t%(total)\n" -p "this month" bal ^$expenses_account \
-  | sed -e 's/[€\$]//g' -e 's/,/./g' \
-  | grep -P -v $month_grep_v \
-  > $build/month_breakdown.txt
 
 python $bin/pie.py "$build/" -f "${ledgerfile}"
